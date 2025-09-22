3f6442b0fe6792bb7be8215716624a6062569f28
Manoah Kinnaert <manoah.k@icloud.com>
2025-09-22 12:52:42 +0200

    Added CDF chart functionality.

--- StatDistributions/ui/views/binomial_dist_view.py
+++ StatDistributions/ui/views/binomial_dist_view.py
@@ -17,18 +17,31 @@ class BinomialDistView(tk.Frame):
         self.n.trace_add("write", lambda *args: self.plot_dist(*args))
         self.p.trace_add("write", lambda *args: self.plot_dist(*args))
 
-        self.figure = Figure(figsize=(5, 5), dpi=100)
-        self.pmf_plot = self.figure.add_subplot(111)
-        self.pmf_plot.set_title("Probability Mass Function")
         
+        self.figure1 = Figure(figsize=(5, 5), dpi=100)
+        self.pmf_plot = self.figure1.add_subplot(111)
+        self.pmf_plot.set_title("PMF (Probability Mass Function)")
+        
+        self.figure2 = Figure(figsize=(5, 5), dpi=100)
+        self.cdf_plot = self.figure2.add_subplot(111)
+        self.cdf_plot.set_title("CDF (Cumulative Distribution Function)")
+
+
+
         self.setup_ui()
     
     def setup_ui(self):
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
-        self.canvas.get_tk_widget().pack(fill="both", expand=True)
+        self.chart_frame = tk.Frame(self, bg=self["background"])
+        self.chart_frame.pack(side="top", fill="both")
+
+        self.canvas1 = FigureCanvasTkAgg(self.figure1, master=self.chart_frame)
+        self.canvas1.get_tk_widget().pack(side="left", fill="both", expand=True)
+
+        self.canvas2 = FigureCanvasTkAgg(self.figure2, master=self.chart_frame)
+        self.canvas2.get_tk_widget().pack(side="right", fill="both", expand=True)
 
         self.input_frame = tk.Frame(self, bg=self["background"])
-        self.input_frame.pack(side="bottom", fill="both", expand=True)
+        self.input_frame.pack(side="bottom", fill="x")
 
         self.n_input_frame = tk.Frame(self.input_frame, bg=self["background"])
         self.n_input_frame.grid(row=0, column=0)
@@ -47,6 +60,7 @@ class BinomialDistView(tk.Frame):
         try:
             n, p = self.n.get(), self.p.get()
             self.pmf_plot.cla()
+            self.cdf_plot.cla()
         except:
             return 
         
@@ -58,6 +72,10 @@ class BinomialDistView(tk.Frame):
         self.pmf_plot.set_title("PMF (Probability Mass Function)")
         
         # plot the cdf
+        y = binom.cdf(k, n, p)
+        self.cdf_plot.plot(k, y, "go", ms=8)
+        self.cdf_plot.hlines(y, 0, k, colors="g", lw=2, alpha=0.2)
+        self.cdf_plot.set_title("CDF (Cumulative Distribution Function)")
         
-        self.canvas.draw()
-
+        self.canvas1.draw()
+        self.canvas2.draw()
