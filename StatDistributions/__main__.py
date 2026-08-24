1c4078ee2b38bc7ce7ec55c9fe743bd00f77d9a8
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-24 12:43:12 +0200

    Starter code

--- StatDistributions/__main__.py
+++ StatDistributions/__main__.py
@@ -1,4 +1,12 @@
+from flask import Flask, render_template
 import webview 
 
+server = Flask(__name__, static_folder="./static", template_folder="./templates")
+
+@server.route("/")
+def main():
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    pass 
+    webview.create_window("StatDistributions", server)
+    webview.start()
