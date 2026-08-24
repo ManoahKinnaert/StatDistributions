070ec8aa865118065f857e562f8a9d4317900c1e
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-24 14:25:06 +0200

    distribution calculations

--- StatDistributions/__main__.py
+++ StatDistributions/__main__.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, jsonify
 import webview 
+
+import numpy as np 
+from scipy.stats import norm, binom
 
 server = Flask(__name__, static_folder="./static", template_folder="./templates")
 
@@ -14,6 +17,30 @@ def binom_dist():
 @server.route("/")
 def main():
     return redirect("/normalDist")
+
+@server.route("/binom_calc")
+def calculate_binom():
+    # get the params
+    n = int(request.args.get("n"))
+    p = int(request.args.get("p"))
+    # calculations for plotting pmf
+    k = np.arange(0, n + 1)
+    y_pmf = binom.pmf(k, n, p)
+    # calculations for plotting cdf 
+    y_cdf = binom.cdf(k, n, p)
+    # return a json response
+    return jsonify({"x": k, "y_pmf": y_pmf, "y_cdf": y_cdf})
+    
+@server.route("/normal_calc")
+def calculate_normal():
+    # get the params
+    mu = float(request.args.get("mu"))
+    sigma = float(request.args.get("sigma"))
+    # calculate the points to be shown on the chart
+    x = np.linspace(mu - 3 * sigma - .2, mu + 3 * sigma + .2, 100)
+    y = norm.pdf(x, mu, sigma)
+    # return a json response
+    return jsonify({"x": x, "y": y})
 
 def launch_webview():
     webview.create_window("StatDistributions", server)
